6b827bccff20e51203eec431377449377471a710
Luca Bicego <30547111+lucabicego@users.noreply.github.com>
2023-07-12 16:20:26 +0200

    Modifiche effettuate durante le lezioni

--- ball.py
+++ ball.py
@@ -2,24 +2,39 @@ from turtle import Screen, Turtle
 
 
 class Ball(Turtle):
-    def __init__(self,position):
+    def __init__(self,positionR):
         super().__init__()
+        self.position=positionR
         self.shape("circle")
         self.shapesize(stretch_wid=1,stretch_len=1)
         self.penup()
         self.color("white")
         self.fillcolor("white")
-        self.goto(position)
+        self.goto(self.position)
         self.inc_x=10
         self.inc_y=10
+        self.move_speed=0.1
 
     def move(self):
         new_x=self.xcor()
         new_y=self.ycor()
-        if (new_x +self.inc_x >= 390 or new_x +self.inc_x <= -390):
-            self.inc_x *=-1
+        #if (new_x +self.inc_x >= 390 or new_x +self.inc_x <= -390):
+        #    self.bounce_x()
         if (new_y +self.inc_y >= 290 or new_y +self.inc_y <= -290):
-            self.inc_y *=-1
+            self.bounce_y()
         new_x +=self.inc_x
         new_y +=self.inc_y
         self.goto(new_x,new_y)
+
+    def bounce_y(self):
+        self.inc_y *= -1
+
+    def bounce_x(self):
+        self.inc_x *= -1
+        self.move_speed *=0.9
+    def reset_position(self):
+        self.goto(self.position)
+        self.bounce_x()
+        self.move_speed =0.1
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen,Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen=Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)
 paddleRight=Paddle((350,0))
 paddleLeft=Paddle((-350,0))
 ball=Ball((0,0))
+scoreboard=Scoreboard()
 
 
 screen.listen()
@@ -25,11 +27,26 @@ screen.onkeypress(paddleLeft.go_down,"s")
 game_is_on=True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
-
-
+    #Detect collision with right paddle
+    if ball.distance(paddleRight) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+    #Detect collision with left paddle
+    elif ball.distance(paddleLeft) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    #Detect when the ball is out on the left and right
+    elif ball.xcor() < -390 :
+        scoreboard.r_point()
+        ball.reset_position()
+        screen.update()
+        time.sleep(0.5)
+    elif ball.xcor() > 390:
+        scoreboard.l_point()
+        ball.reset_position()
+        screen.update()
+        time.sleep(0.5)
 
 screen.exitonclick()
 
